ebc843847d68365da32f23c64c246696630abbcd
SithilSemitha <sithilsemithad@gmail.com>
2026-03-06 19:29:06 +0530

    feat: Added document tsx page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <title>MyNotes</title>
+      <link rel="icon" href="/favicon.ico" />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
