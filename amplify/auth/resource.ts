c1a600c31bccd70a069be97ce26e0eeab90a0f61
SithilSemitha <sithilsemithad@gmail.com>
2026-03-18 22:18:00 +0530

    fix: Updated resource tsx files

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,10 +1,7 @@
 import { defineAuth } from '@aws-amplify/backend';
 import { postConfirmation } from '../triggers/postConfirmation/resource'
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
+
 export const auth = defineAuth({
   loginWith: {
     email: true,
